5dd8d62098b36344ace756149c815db915ccd0a5
harshalmal <h.malokar24@gmail.com>
2021-11-08 20:41:16 +0530

    environment created & ignore new file and folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  data: "I am Production Environment",
+  envName: "Production"
 };
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  data: "I am local Environment",
+  envName: "local"
 };
 
 /*
